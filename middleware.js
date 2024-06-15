9b219edf1ab345f36ba3fa7ebdeb5c7862821d14
Gerard Homsi <gerard.homsi@gmail.com>
2024-06-15 17:28:51 +0300

    authh

--- middleware.js
+++ middleware.js
@@ -1,3 +1,27 @@
+// import { getToken } from "next-auth/jwt";
+// import { NextResponse } from "next/server";
+
+// export async function middleware(req) {
+//   const protectedRoutes = ["/projects", "/projects/*", "/addAdmin"];
+
+//   const url = req.nextUrl.clone();
+//   const isProtectedRoute = protectedRoutes.some((route) => new RegExp(`^${route.replace("*", ".*")}$`).test(url.pathname));
+
+//   if (isProtectedRoute) {
+//     const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+//     console.log("tokennnnnnnnnnnnnnn", token);
+
+//     if (!token) {
+//       url.pathname = "/login";
+//       return NextResponse.redirect(url);
+//     }
+//   }
+
+//   return NextResponse.next();
+// }
+
+// export const config = { matcher: ["/projects/:path*", "/addAdmin"] };
+
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
@@ -11,10 +35,13 @@ export async function middleware(req) {
     const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
     console.log("tokennnnnnnnnnnnnnn", token);
 
-    // if (!token) {
-    //   url.pathname = "/login";
-    //   return NextResponse.redirect(url);
-    // }
+    // Access token directly from the returned token object
+    const userToken = token?.accessToken; // Optional chaining to handle potential null token
+
+    if (!userToken) {
+      url.pathname = "/login";
+      return NextResponse.redirect(url);
+    }
   }
 
   return NextResponse.next();
